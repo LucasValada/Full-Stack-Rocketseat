7fcda61d05db01c91db890dc968e16b52f26dd7f
Lucas Valadão <valadaolucas6@gmail.com>
2025-09-22 20:25:20 -0300

    FEAT: Insere novas aulas de estilos com javascript

---
+++ Full-Stack/JavaScript/DOM/template/styles.js
@@ -0,0 +1,11 @@
+const input = document.querySelector("#name");
+// Adiciona uma classe via JS
+input.classList.add("input-error");
+// Remove uma classe via JS
+input.classList.remove("input-error");
+// Verifica se o elemento possui a classe, se nao tiver ele adiciona, se tiver ele remove
+input.classList.toggle("input-error");
+
+const button = document.querySelector("#btn");
+button.style.backgroundColor = "yellow";
+button.style.color = "black";
