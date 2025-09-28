e3a23bcd776bb7f820833eaf071e19137b36713a
Lucas Valadão <valadaolucas6@gmail.com>
2025-09-28 18:24:15 -0300

    FEAT: Insere novas aulas e exemplos de como usar regexr

--- Full-Stack/JavaScript/DOM/eventos/eventInput.js
+++ Full-Stack/JavaScript/DOM/eventos/eventInput.js
@@ -1,15 +1,15 @@
-const input = document.querySelector("input");
-// keydown - quando a tecla é pressionada (captura tudo, incluindo CTRL, SHIFT, ALT, etc)
-// input.addEventListener("keydown", (event) => {
+// const input = document.querySelector("input");
+// // keydown - quando a tecla é pressionada (captura tudo, incluindo CTRL, SHIFT, ALT, etc)
+// // input.addEventListener("keydown", (event) => {
+// //   console.log(event.key);
+// // });
+
+// // keypress - quando a tecla solta é do tipo caracter
+// input.addEventListener("keypress", (event) => {
 //   console.log(event.key);
 // });
 
-// keypress - quando a tecla solta é do tipo caracter
-input.addEventListener("keypress", (event) => {
-  console.log(event.key);
-});
-
-// quando o conteudo muda
-input.onchange = (event) => {
-  console.log("O INPUT MUDOU!");
-};
+// // quando o conteudo muda
+// input.onchange = (event) => {
+//   console.log("O INPUT MUDOU!");
+// };
---
+++ Full-Stack/JavaScript/DOM/eventos/regexrInput.js
@@ -0,0 +1,26 @@
+const input = document.querySelector("input");
+// input.addEventListener("input", (event) => {
+//   const regex = /\D+/g;
+//   const value = input.value;
+
+//   //Retorna o padrao encotrando na string
+//   //   console.log(value.match(regex));
+
+//   // Verifica se atende ou nao o padrao
+//   //   const isValid = regex.test(value);
+//   //   console.log(isValid);
+// });
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const value = input.value;
+  const regex = /\D+/g;
+  if (regex.test(value)) {
+    alert("Padrão encontrado");
+  } else {
+    alert("Padrão não encontrado");
+  }
+  //   const value = input.value.replace(regex, "");
+  //   console.log(value);
+});
