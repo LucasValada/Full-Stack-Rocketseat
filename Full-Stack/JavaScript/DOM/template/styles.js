93d56ff799401ec8cb04529ae867554bc720839a
Lucas Valadão <valadaolucas6@gmail.com>
2025-09-28 14:05:09 -0300

    FIX: Ajusta branch de JAVASCRIPT e pastas do projeto, adiciona um gitignore

--- Full-Stack/JavaScript/DOM/template/styles.js
+++ Full-Stack/JavaScript/DOM/template/styles.js
@@ -1,10 +1,10 @@
 const input = document.querySelector("#name");
 // Adiciona uma classe via JS
-input.classList.add("input-error");
+// input.classList.add("input-error");
 // Remove uma classe via JS
-input.classList.remove("input-error");
+// input.classList.remove("input-error");
 // Verifica se o elemento possui a classe, se nao tiver ele adiciona, se tiver ele remove
-input.classList.toggle("input-error");
+// input.classList.toggle("input-error");
 
 const button = document.querySelector("#btn");
 button.style.backgroundColor = "yellow";
