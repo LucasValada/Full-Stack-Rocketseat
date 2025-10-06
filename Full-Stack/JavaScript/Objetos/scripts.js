ac12a999bc809718f67162a631d68e1692033e42
Lucas Valadão <valadaolucas6@gmail.com>
2025-10-06 19:30:22 -0300

    FEAT: Finaliza a primeira aula de objeto e cria objetos de teste para acesso

--- Full-Stack/JavaScript/Objetos/scripts.js
+++ Full-Stack/JavaScript/Objetos/scripts.js
@@ -6,3 +6,28 @@
 
 // Criando um objeto vazio
 const obj = {};
+console.log(obj);
+
+// Cria um objeto com propriedades e métodos.
+const user = {
+  // chave e valor
+  email: "lucas@email.com",
+  // : serve para atribuir o valor dentro de um objeto e nao o =, nao precisa estar dentro de aspas a chave
+  age: 23,
+  name: {
+    // estrutura de propriedades aninhadas, um objeto dentro de outro.
+    firstName: "Lucas",
+    surname: "Valadão",
+  },
+  address: {
+    location: "Rua X",
+    number: 123,
+    city: "Sao Paulo",
+    postal_code: "12345-000",
+  },
+  message: () => {
+    console.log("Olá, é bom ter você aqui!");
+  },
+};
+
+// Acessando propriedades e métodos usando a notação de ponto.
