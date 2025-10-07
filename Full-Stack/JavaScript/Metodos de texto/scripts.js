944c2851fd269473d4503d27c1bf17d4e1a91185
Lucas Valadão <valadaolucas6@gmail.com>
2025-10-07 20:17:10 -0300

    feat: adiciona estrutura das aulas de metodos de texto, cria js alterando texto para Uppercase e Lowercase

---
+++ Full-Stack/JavaScript/Metodos de texto/scripts.js
@@ -0,0 +1,9 @@
+let message = "Estou estudando os fundamentos do JavaScript";
+
+console.log(message);
+
+// Exibir o testo em maiúsculo
+console.log(message.toUpperCase());
+
+// Exibir o texto em minúsculo
+console.log(message.toLowerCase());
