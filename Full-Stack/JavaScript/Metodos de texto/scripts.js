368d3ab57459447af6fe093d83d49ab64bf01a1d
Lucas Valadão <valadaolucas6@gmail.com>
2025-10-07 20:21:17 -0300

    feat: obtendo o comprimento de uma string

--- Full-Stack/JavaScript/Metodos de texto/scripts.js
+++ Full-Stack/JavaScript/Metodos de texto/scripts.js
@@ -7,3 +7,16 @@ console.log(message.toUpperCase());
 
 // Exibir o texto em minúsculo
 console.log(message.toLowerCase());
+
+// Exibir o tamanho do texto
+console.log(message.length);
+
+let password = "12345";
+if (password.length < 6) {
+  console.log("A senha deve ter pelo menos 6 caracteres");
+}
+
+// Quantos digitos tem um número
+let value = 12345;
+console.log(value.toString().length);
+console.log(String(value + "teste 2").length);
