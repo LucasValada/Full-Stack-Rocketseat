81e7b160893f1c3f1244c604474970f6ba0df6ac
Lucas Valadão <valadaolucas6@gmail.com>
2025-10-08 17:17:51 -0300

    feat: Cria estrutura das aulas de array

---
+++ Full-Stack/JavaScript/Arrays/scripts.js
@@ -0,0 +1,13 @@
+// Criando Array com construtor.
+const newArray = new Array();
+// [] - Array
+// {} - Objeto
+// () - Funcao
+console.log(newArray);
+
+// Retorna o tamanho do array
+console.log(newArray.length);
+
+const available = new Array(10);
+console.log(available.length); // mostra 10 posições vazias
+console.log(available);
