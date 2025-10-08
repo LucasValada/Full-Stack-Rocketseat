a319f20214bc77912ef2e6f4b86cd90387620452
Lucas Valadão <valadaolucas6@gmail.com>
2025-10-08 17:24:39 -0300

    feat : Criando e acessando um array

--- Full-Stack/JavaScript/Arrays/scripts.js
+++ Full-Stack/JavaScript/Arrays/scripts.js
@@ -11,3 +11,15 @@ console.log(newArray.length);
 const available = new Array(10);
 console.log(available.length); // mostra 10 posições vazias
 console.log(available);
+
+let fruits = ["Banana", "Orange", "Apple", "Watermelon"];
+console.log(fruits);
+
+// Acessando o array pelo indice
+console.log(fruits[1]);
+
+// Acessando o array pelo indice inexistente
+console.log(fruits[7]);
+
+// Acessando de forma dinamica o ultimo item
+console.log(fruits[fruits.length - 1]);
