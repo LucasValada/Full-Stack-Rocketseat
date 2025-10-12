edd72f3a6a1a1d3002f28d34e2a431f66a568b21
Lucas Valadão <valadaolucas6@gmail.com>
2025-10-12 16:48:45 -0300

    FEAT: define data e hora espeficifica e data e hora com string

--- Full-Stack/JavaScript/Data e Hora/getDataHour.js
+++ Full-Stack/JavaScript/Data e Hora/getDataHour.js
@@ -9,3 +9,14 @@ console.log(date.getMinutes());
 console.log(date.getSeconds());
 console.log(date.getMilliseconds());
 console.log(date.getTime());
+
+// Definindo uma data especifica com ano, mes(0-11) e dia.
+const date2 = new Date(2025, 9, 12);
+console.log(date2);
+
+// Define data e hora
+console.log(new Date(2025, 9, 12, 16, 25, 0));
+
+// definindo data e hora com string
+console.log(new Date("2025-10-12T16:25:00"));
+console.log(new Date("October 12, 2025 16:25:00"));
