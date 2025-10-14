1e6a917dbeefb2277cf0f58cf168f46b2e3c9479
Lucas Valadão <valadaolucas6@gmail.com>
2025-10-14 18:57:37 -0300

    FEAT: Obtendo os dados do form e formatando a moeda pro padrão BRL

--- Full-Stack/Projetos Estudos/Javascript/Refund/scripts.js
+++ Full-Stack/Projetos Estudos/Javascript/Refund/scripts.js
@@ -1,4 +1,28 @@
-const amout = document.getElementById("amount");
-amount.onpinput = () => {
-    
+// Seleciona os elementos do formulário
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+// Capture o evento de input para formatar o valor
+amount.oninput = () => {
+  // Obtém o valor atual e remove tudo que não for dígito
+  let value = amount.value.replace(/\D/g, "");
+
+  // Tranformar o valor em centavos
+  value = Number(value) / 100;
+  // Atualiza o valor do campo com o valor limpo
+  amount.value = formatCurrencyBRL(value);
+};
+
+function formatCurrencyBRL(value) {
+  // Formata o valor como moeda brasileira (BRL)
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+  return value;
+}
+
+form.onsubmit = (event) => {
+  event.preventDefault();
 };
