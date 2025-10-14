0321f9c9d9533f0ccd7bee08f3253ec7f0472e67
Lucas Valadão <valadaolucas6@gmail.com>
2025-10-14 19:16:21 -0300

    FEAT: Cria a function de adicionar um item na lista com as categorias e classes css dinamicas

--- Full-Stack/Projetos Estudos/Javascript/Refund/scripts.js
+++ Full-Stack/Projetos Estudos/Javascript/Refund/scripts.js
@@ -3,6 +3,9 @@ const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const expense = document.getElementById("expense");
 const category = document.getElementById("category");
+
+// Seleciona os elemetos da lista
+const expenseList = document.querySelector("ul");
 // Capture o evento de input para formatar o valor
 amount.oninput = () => {
   // Obtém o valor atual e remove tudo que não for dígito
@@ -36,5 +39,28 @@ form.onsubmit = (event) => {
     amount: amount.value,
     create_at: new Date(),
   };
-  console.log(newExpense);
+
+  // Chama a função para adicionar a despesa na lista
+  expenseAdd(newExpense);
 };
+
+function expenseAdd(newExpense) {
+  try {
+    // Cria o elemento li para a nova despesa
+    const expenseItem = document.createElement("li"); // Cria o elemento li
+    expenseItem.classList.add("expense"); // Adiciona a classe CSS
+
+    // Cria o icone da categoria
+    const expenseIcon = document.createElement("img"); // Cria o elemento img
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`); // Define o src da imagem
+    expenseIcon.setAttribute("alt", newExpense.category_name); // Define o alt da imagem
+    expenseIcon.classList.add("expense-icon"); // Adiciona a classe CSS
+
+    // Adiciona as informações no item.
+    expenseItem.append(expenseIcon);
+    // Adiciona o item na lsita
+    expenseList.append(expenseItem);
+  } catch (error) {
+    alert("Erro ao adicionar despesa: " + error.message);
+  }
+}
