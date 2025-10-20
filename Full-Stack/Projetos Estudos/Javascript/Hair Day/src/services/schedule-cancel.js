2500728dfe0a1122eca5de3c2f339976d15c584c
Lucas Valadão <valadaolucas6@gmail.com>
2025-10-19 22:23:12 -0300

    FEAT: Ajuste no hair day, ajusta a exclusão de agendamentos marcados

--- Full-Stack/Projetos Estudos/Javascript/Hair Day/src/services/schedule-cancel.js
+++ Full-Stack/Projetos Estudos/Javascript/Hair Day/src/services/schedule-cancel.js
@@ -1,14 +1,13 @@
 import { apiConfig } from "./api-config";
+
 export async function scheduleCancel(id) {
   try {
     await fetch(`${apiConfig.baseUrl}/schedules/${id}`, {
       method: "DELETE",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
     });
     alert("Agendamento cancelado com sucesso!");
   } catch (error) {
-    alert("Erro ao cancelar o agendamento. Tente novamente mais tarde.");
+    console.error("Erro ao cancelar:", error);
+    alert("Erro ao cancelar agendamento.");
   }
 }
