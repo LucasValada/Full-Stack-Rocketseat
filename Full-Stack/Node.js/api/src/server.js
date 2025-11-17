6c514b231e83e601e2ae18590ac4a6519d69602b
Lucas Valadão <valadaolucas6@gmail.com>
2025-11-17 15:20:49 -0300

    feat: recuperando dados no body

--- Full-Stack/Node.js/api/src/server.js
+++ Full-Stack/Node.js/api/src/server.js
@@ -1,7 +1,22 @@
 import http from "node:http";
 
-const server = http.createServer((req, res) => {
-  return res.end("Resposta do meu primeiro servidor");
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/products") {
+    return res.end("Listagem de produtos");
+  }
+  if (method === "POST" && url === "/products") {
+    const buffers = [];
+    for await (const chunk of req) {
+      buffers.push(chunk);
+    }
+    console.log(Buffer.concat(buffers).toString());
+
+    return res.writeHead(201).end("Produto cadastrado com sucesso");
+  }
+  return res.writeHead(404).end("Rota nao encontrada");
+  // return res.end("Método HTTP: " + method);
 });
 
 server.listen(3333);
