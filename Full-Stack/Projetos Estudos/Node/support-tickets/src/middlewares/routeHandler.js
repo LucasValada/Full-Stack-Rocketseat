d1599c8caec4250db73f197d01e62b60a55c189a
Lucas Valadão <valadaolucas6@gmail.com>
2025-12-04 15:52:37 -0300

    feat: parametros dinamicos

--- Full-Stack/Projetos Estudos/Node/support-tickets/src/middlewares/routeHandler.js
+++ Full-Stack/Projetos Estudos/Node/support-tickets/src/middlewares/routeHandler.js
@@ -4,7 +4,7 @@ import { routes } from "../routes/index.js";
 const database = new Database();
 export function routeHandler(request, response) {
   const route = routes.find((route) => {
-    return route.method === request.method && route.path === request.url;
+    return route.method === request.method && route.path.test(request.url);
   });
   if (route) {
     return route.controller({ request, response, database });
