154d4702999aaa7646e503efb6bdcee7e3926249
Lucas Valadão <valadaolucas6@gmail.com>
2025-12-18 12:59:34 -0300

    feat: enviando req com o metodo de get

--- Full-Stack/Node.js/api-rest/src/server.ts
+++ Full-Stack/Node.js/api-rest/src/server.ts
@@ -2,4 +2,8 @@ import express from "express";
 const PORT = 3333;
 const app = express();
 
+app.get("/", (request, response) => {
+  response.send("Hello World");
+});
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
