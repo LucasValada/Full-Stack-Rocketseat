324a05f1ff7c1767264149a194ee440d788fbd59
Lucas Valadão <valadaolucas6@gmail.com>
2025-12-18 13:04:18 -0300

    feat: passando parametros nas rotas e recuperando

--- Full-Stack/Node.js/api-rest/src/server.ts
+++ Full-Stack/Node.js/api-rest/src/server.ts
@@ -2,8 +2,9 @@ import express from "express";
 const PORT = 3333;
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("Hello World");
+app.get("/products/:id/:user", (request, response) => {
+  const { id, user } = request.params;
+  response.send(`Produto ${id} encontrado para o usuário ${user}`);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
