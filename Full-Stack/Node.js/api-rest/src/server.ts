a36c594efba05ed70e75fac704dd54fbb8e20dbe
Lucas Valadão <valadaolucas6@gmail.com>
2025-12-18 11:47:44 -0300

    feat: insere o framework do express e cria uma porta local

--- Full-Stack/Node.js/api-rest/src/server.ts
+++ Full-Stack/Node.js/api-rest/src/server.ts
@@ -1,5 +1,5 @@
-function sum(a: number, b: number): number {
-  return a + b;
-}
-const result: number = sum(1, 2);
-console.log(result);
+import express from "express";
+const PORT = 3333;
+const app = express();
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
