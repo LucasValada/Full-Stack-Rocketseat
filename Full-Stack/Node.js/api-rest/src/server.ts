6493bda5cb6d8986a14cd05fc38c21c5ade3bbec
Lucas Valadão <valadaolucas6@gmail.com>
2026-01-02 18:32:53 -0300

    feat: criando os controllers

--- Full-Stack/Node.js/api-rest/src/server.ts
+++ Full-Stack/Node.js/api-rest/src/server.ts
@@ -1,22 +1,7 @@
 import express from "express";
-import { myMiddleware } from "./middlewares/my-middleware";
+import { routes } from "./routes";
 const PORT = 3333;
 const app = express();
 app.use(express.json());
-// middleware global aplica em todas as rotas abaixo.
-// app.use(myMiddleware);
-
-app.get("/products", (request, response) => {
-  // /products?page=1&limit=10
-  const { page, limit } = request.query;
-  response.send(`pagina ${page} de ${limit}`);
-});
-
-// middleware local aplica em uma rota específica.
-app.post("/products", myMiddleware, (request, response) => {
-  const { name, price } = request.body;
-  // response.send(`Produto '${name}' criado com sucesso. valor: ${price}`);
-  response.status(201).json({ name, price, user_id: request.user_id });
-});
-
+app.use(routes);
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
