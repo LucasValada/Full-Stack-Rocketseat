4aa96134ecfd8fe2726fb72634330f1c728fd9d0
Lucas Valadão <valadaolucas6@gmail.com>
2026-01-02 15:59:09 -0300

    feat: aplicando middleware global e local

--- Full-Stack/Node.js/api-rest/src/server.ts
+++ Full-Stack/Node.js/api-rest/src/server.ts
@@ -1,14 +1,19 @@
 import express from "express";
+import { myMiddleware } from "./middlewares/my-middleware";
 const PORT = 3333;
 const app = express();
 app.use(express.json());
+// middleware global aplica em todas as rotas abaixo.
+// app.use(myMiddleware);
+
 app.get("/products", (request, response) => {
   // /products?page=1&limit=10
   const { page, limit } = request.query;
   response.send(`pagina ${page} de ${limit}`);
 });
 
-app.post("/products", (request, response) => {
+// middleware local aplica em uma rota específica.
+app.post("/products", myMiddleware, (request, response) => {
   const { name, price } = request.body;
   // response.send(`Produto '${name}' criado com sucesso. valor: ${price}`);
   response.status(201).json({ name, price });
