840c5da25a1d4e3505baec72e36d7bba1b08679f
Lucas Valadão <valadaolucas6@gmail.com>
2026-01-02 15:46:01 -0300

    feat: dados no corpo da requisição

--- Full-Stack/Node.js/api-rest/src/server.ts
+++ Full-Stack/Node.js/api-rest/src/server.ts
@@ -1,11 +1,16 @@
 import express from "express";
 const PORT = 3333;
 const app = express();
-
+app.use(express.json());
 app.get("/products", (request, response) => {
   // /products?page=1&limit=10
   const { page, limit } = request.query;
   response.send(`pagina ${page} de ${limit}`);
+});
+
+app.post("/products", (request, response) => {
+  const { name, price } = request.body;
+  response.send(`Produto '${name}' criado com sucesso. valor: ${price}`);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
