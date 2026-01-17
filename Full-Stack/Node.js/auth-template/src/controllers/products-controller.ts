070edada64321691204ec0bd50950e21b7d4932f
Lucas Valadão <valadaolucas6@gmail.com>
2026-01-17 20:48:27 -0300

    feat: Definindo papeis de usuario e criando/extraindo token de usuario

--- Full-Stack/Node.js/auth-template/src/controllers/products-controller.ts
+++ Full-Stack/Node.js/auth-template/src/controllers/products-controller.ts
@@ -6,7 +6,7 @@ class ProductsController {
   }
 
   async create(request: Request, response: Response) {
-    return response.status(201).json({ message: "Product created" });
+    return response.status(201).json({ message: request.user?.role });
   }
 }
 
--- Full-Stack/Node.js/auth-template/src/controllers/sessions-controller.ts
+++ Full-Stack/Node.js/auth-template/src/controllers/sessions-controller.ts
@@ -1,8 +1,28 @@
 import { Request, Response } from "express";
-
+import { AppError } from "@/utils/AppError";
+import { sign } from "jsonwebtoken";
+import { authConfig } from "@/configs/auth";
 class SessionsController {
   async create(request: Request, response: Response) {
-    return response.json({ message: "Session created successfully!" });
+    const { username, password } = request.body;
+    const fakeUser = {
+      id: "1",
+      username: "Lucas Valadao",
+      password: "123456",
+      role: "customer",
+    };
+
+    if (username !== fakeUser.username || password !== fakeUser.password) {
+      throw new AppError("Username or password incorrect!", 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+    const token = sign({ role: fakeUser.role }, secret, {
+      subject: String(fakeUser.id),
+      expiresIn,
+    });
+
+    return response.json({ token });
   }
 }
 
