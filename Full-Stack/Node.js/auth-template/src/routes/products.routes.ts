75fd1f9a49f516f1234abaedb91c3c50d75b79cd
Lucas Valadão <valadaolucas6@gmail.com>
2026-01-17 21:06:14 -0300

    feat: encerra o projeto e cria autenticação e autorização por roles

--- Full-Stack/Node.js/auth-template/src/routes/products.routes.ts
+++ Full-Stack/Node.js/auth-template/src/routes/products.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { ProductsController } from "@/controllers/products-controller";
 import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated";
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
 const productsRoutes = Router();
 const productsController = new ProductsController();
 
+productsRoutes.use(verifyUserAuthorization(["admin", "customer"]));
 productsRoutes.get("/", productsController.index);
 productsRoutes.post("/", ensureAuthenticated, productsController.create);
 
