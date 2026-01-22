14072dfb3ccf5fe8c2085a2a72461496a47c234e
Lucas Valadão <valadaolucas6@gmail.com>
2026-01-22 15:18:56 -0300

    feat: cadastra o log

--- Full-Stack/Projetos Estudos/Rocket Log/src/controllers/delivery-logs-controller.ts
+++ Full-Stack/Projetos Estudos/Rocket Log/src/controllers/delivery-logs-controller.ts
@@ -1,7 +1,36 @@
 import { Request, Response } from "express";
+import { z } from "zod";
+import { prisma } from "@/database/prisma";
+import { appError } from "@/utils/appError";
 
 export class DeliveryLogsController {
   async create(request: Request, response: Response) {
-    return response.json({ message: "Create delivery log" });
+    const bodySchema = z.object({
+      delivery_id: z.string().uuid(),
+      description: z.string(),
+    });
+
+    const { delivery_id, description } = bodySchema.parse(request.body);
+
+    const delivery = await prisma.delivery.findUnique({
+      where: { id: delivery_id },
+    });
+
+    if (!delivery) {
+      throw new appError("Delivery not found", 404);
+    }
+
+    if (delivery.status === "processing") {
+      throw new appError("change the status of the delivery", 400);
+    }
+
+    await prisma.deliveryLog.create({
+      data: {
+        deliveryId: delivery_id,
+        description,
+        status: delivery.status,
+      },
+    });
+    return response.status(201).json();
   }
 }
